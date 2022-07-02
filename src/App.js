60ae15a05372cfa57dec07b1549b0e95eafccad8
xujuanchen2020 <xujuan.chen@lc.cuny.edu>
2022-07-02 13:27:25 -0400

    useState counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/4-useState-object'
-import Final from './tutorial/1-useState/final/4-useState-object'
+import Setup from './tutorial/1-useState/setup/5-useState-counter'
+import Final from './tutorial/1-useState/final/5-useState-counter'
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
       <Setup />
-      {/* <Final /> */}
+      <Final />
     </div>
   )
 }
