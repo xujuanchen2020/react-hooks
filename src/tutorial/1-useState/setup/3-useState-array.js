a5003e833d738f1b3e5dbbdb143197e33d1b4662
xujuanchen2020 <xujuan.chen@lc.cuny.edu>
2022-07-02 11:04:01 -0400

    useState Array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const handleRemove = (id) => {
+    let newPeople = people.filter((person)=> person.id !== id)
+    setPeople(newPeople);
+  }
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button type="button" onClick={() => handleRemove(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button type="button" className="btn" onClick={() => setPeople([])}>
+        Clear
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
