67cba702d99f3d62b0aaeafc4556e188f070d650
xujuanchen2020 <xujuan.chen@lc.cuny.edu>
2022-07-02 18:04:55 -0400

    useEffect window size

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,17 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  }
+  useEffect(()=>{
+    window.addEventListener('resize', checkSize);
+    return () => (
+      window.removeEventListener('resize', checkSize)
+    )
+  },[])
+  return <h2>{size} px</h2>;
 };
 
 export default UseEffectCleanup;
