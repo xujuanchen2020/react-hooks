3fe90c37be3a5d765987e8c0b893814b78d8a590
xujuanchen2020 <xujuan.chen@lc.cuny.edu>
2022-07-02 21:45:19 -0400

    form contol input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide'
-import Final from './tutorial/3-conditional-rendering/final/3-show-hide'
+import Setup from './tutorial/4-forms/setup/1-controlled-inputs'
+import Final from './tutorial/4-forms/final/1-controlled-inputs'
 function App() {
   return (
     <div className='container'>
       <Setup />
-      <Final />
+      {/* <Final /> */}
     </div>
   )
 }
